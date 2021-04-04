37b96d32dfd253e3d13dfe98237216db1506eb1c
Samantha Ostrowski <isphinxs@hotmail.com>
2021-04-04 18:36:23 -0400

    commit tree 5a20561690d8f40a60c3b4ffbe027bde7a71e6f2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Artist.create([{ name: "Adele" }, { name: "The Beatles" }])
+Song.create([{ title: "Hello" }, { title: "Yellow Submarine" }])
